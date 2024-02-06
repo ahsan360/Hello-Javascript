9b979000438f96a73635c7455d220810134ee041
Ahsan <ahsanhabib462@gmail.com>
2024-02-06 18:33:06 +0600

    Map & frequency array like c++

--- day1.js
+++ day1.js
@@ -1,5 +1,16 @@
-import {k,text,settxt} from "./sum.js"; // note that we have the freedom to use import m instead of import k, because k was default export
-console.log(k(1,2,3,4)); // 12
-console.log(text)
-settxt("set from day1")
-console.log(text)
+let myArray = [1, 2, 3, 2, 1, 3, 1, 4];
+let frequencyMap = new Map();
+
+// Counting frequency of each element in the array
+myArray.forEach((element) => {
+  if (frequencyMap.has(element)) {
+    frequencyMap.set(element, frequencyMap.get(element) + 1);
+  } else {
+    frequencyMap.set(element, 1);
+  }
+});
+
+// Displaying the frequency map
+frequencyMap.forEach((count, element) => {
+  console.log(`${element}: ${count} times`);
+});
