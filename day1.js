2ca6785403effe45eb4eb01b457818d9e7ae864c
Ahsan <ahsanhabib462@gmail.com>
2024-02-06 13:31:52 +0600

    destruction and template literal

--- day1.js
+++ day1.js
@@ -1,3 +1,14 @@
-console.log("hello 2nd commit")
-a = [1,2,3,5,6]
-for(i in a)console.log(i)
+//template literal
+let name = "ahsan"
+//console.log(`this is my name ${name}`)
+
+//Destructuring Assignment:
+let a = [1,2,3,4,5,6]
+const [c,d] = a
+obj = {
+    id : 1,
+    s : 'ahsan',
+    first : {d : 1,c : 5}
+}
+const {first : {c : k}} = obj
+console.log(k)
