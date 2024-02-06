f4c7118343ffdc53bd1c7c5c645b211ae7c8186c
Ahsan <ahsanhabib462@gmail.com>
2024-02-06 10:38:35 +0600

    day2

--- day1.js
+++ day1.js
@@ -1,22 +1,3 @@
-function Obj(){
-    this.draw = function(){
-        console.log("hello")
-    }
-}
-
-obj = new Obj()
-console.log(obj)
-
-class Person{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-
-    displayInfo() {
-        console.log(`Name: ${this.name}, Age: ${this.age}`)
-    }
-}
-
-const person1 = new Person("Bob", 25);
-console.log(person1)
+console.log("hello 2nd commit")
+a = [1,2,3,5,6]
+for(i in a)console.log(i)
