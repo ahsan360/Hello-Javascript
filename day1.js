ee5d101fd4977df619e449dc0bac1a8476fafa76
Ahsan <ahsanhabib462@gmail.com>
2024-02-07 11:48:29 +0600

    Try and Catch

--- day1.js
+++ day1.js
@@ -1,16 +1,14 @@
-let myArray = [1, 2, 3, 2, 1, 3, 1, 4];
-let frequencyMap = new Map();
+// program to show try...catch in a program
 
-// Counting frequency of each element in the array
-myArray.forEach((element) => {
-  if (frequencyMap.has(element)) {
-    frequencyMap.set(element, frequencyMap.get(element) + 1);
-  } else {
-    frequencyMap.set(element, 1);
-  }
-});
+const numerator= 100, denominator = 'a';
 
-// Displaying the frequency map
-frequencyMap.forEach((count, element) => {
-  console.log(`${element}: ${count} times`);
-});
+try {
+     console.log(numerator/denominator);
+
+    // forgot to define variable a      
+    console.log(a);
+}
+catch(error) {
+    console.log('An error caught'); 
+    console.log('Error message: ' + error);  
+}
