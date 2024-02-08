9d74ec155527bcbe11d382a08bc051aa7d1ab428
Ahsan <ahsanhabib462@gmail.com>
2024-02-08 15:37:30 +0600

    IFFE with asyn and await

--- day1.js
+++ day1.js
@@ -1,4 +1,17 @@
-let a = [4,7,2,4,2]
-a.sort()
-a.pop()
-console.log(a.lastIndexOf())
+const fun = (val, fn) => {
+    return new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                console.log(`im id number ${val}`)
+                resolve("success")
+                if (fn) fn()
+                
+            }, 2000)
+          
+        })
+    
+}
+
+(async function x (){
+    await fun(1)
+    await fun(3)
+})()
