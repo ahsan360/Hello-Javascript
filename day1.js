c1b5e79597aefc4100e1ea1635057fe3c39ea686
Ahsan <ahsanhabib462@gmail.com>
2024-02-08 13:38:15 +0600

    Find last element

--- day1.js
+++ day1.js
@@ -1,15 +1,4 @@
-fun = (val) =>{
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            console.log(`data ${val}`)
-            resolve()
-        },3000)
-    })
-}
- 
-async function d (){
-    await fun(1);
-    await fun(2);
-    await fun(3);
-}
-d()
+let a = [4,7,2,4,2]
+a.sort()
+a.pop()
+console.log(a.lastIndexOf())
