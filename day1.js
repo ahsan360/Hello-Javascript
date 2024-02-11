8d1b7bba446cf698ad8415f041c124114f1cea70
Ahsan <ahsanhabib462@gmail.com>
2024-02-11 10:20:25 +0600

    test

--- day1.js
+++ day1.js
@@ -1,17 +1,1 @@
-const fun = (val, fn) => {
-    return new Promise((resolve,reject)=>{
-            setTimeout(() => {
-                console.log(`im id number ${val}`)
-                resolve("success")
-                if (fn) fn()
-                
-            }, 2000)
-          
-        })
-    
-}
-
-(async function x (){
-    await fun(1)
-    await fun(3)
-})()
+console.log("test")
